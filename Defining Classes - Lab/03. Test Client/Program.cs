37e2b9f84b99528625fe58304d68bf4769d5726c
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-16 21:08:44 +0200

    Add files via upload

---
+++ Defining Classes - Lab/03. Test Client/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+        var dictionary = new Dictionary<int, BankAccount>();
+
+        var input = Console.ReadLine();
+
+        while (input!="End")
+        {
+            var chars = " ".ToArray();
+            var parts = input.Split(chars, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var command = parts[0];
+            var accountId = int.Parse(parts[1]);
+
+            decimal amount=0;
+            if (parts.Length==3)
+            {
+                amount = decimal.Parse(parts[2]);
+            }
+
+            switch (command)
+            {
+                case "Create":
+                   
+                        if (dictionary.ContainsKey(accountId))
+                        {
+                            Console.WriteLine("Account already exists");
+                        }
+                        else
+                        {
+                            BankAccount bankAccount = new BankAccount(accountId);
+                            dictionary[accountId] = bankAccount;
+                        dictionary[accountId].Id = accountId;
+
+                           // Console.WriteLine(bankAccount.Id);
+                           // Console.WriteLine(bankAccount.Balance);
+                        }
+                    
+                    break;
+
+                case "Deposit":
+                    if (!dictionary.ContainsKey(accountId))
+                    {
+                        Console.WriteLine("Account does not exist");
+                    }
+                    else
+                    {
+                        dictionary[accountId].Balance += amount;
+                        // Console.WriteLine($"{dictionary[accountId].Id} {dictionary[accountId].Balance}");
+
+                    }
+                    break;
+
+                case "Withdraw":
+                    if (!dictionary.ContainsKey(accountId))
+                    {
+                        Console.WriteLine("Account does not exist");
+
+                    }
+                    else if (dictionary.ContainsKey(accountId) && dictionary[accountId].Balance < amount)
+                    {
+                        Console.WriteLine("Insufficient balance");
+                    }
+                    
+                    else if(dictionary.ContainsKey(accountId) && dictionary[accountId].Id==accountId)
+                    {
+                     
+                        dictionary[accountId].Balance -= amount;
+                    }
+                  //  Console.WriteLine($"{dictionary[accountId].Id} {dictionary[accountId].Balance}");
+
+                    break;
+
+                case "Print":
+
+                    if (!dictionary.ContainsKey(accountId))
+                    {
+                        Console.WriteLine("Account does not exist");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{dictionary[accountId].ToString()}");
+
+                    }
+
+                    break;
+            }
+            input = Console.ReadLine();
+        }
+       
+
+
+
+        
+        }
+  
+}
+
+
