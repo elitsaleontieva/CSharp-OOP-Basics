523de739ff730ab6d5927b46c54d5efe44510450
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-16 18:37:54 +0200

    Create BankAccount.cs

---
+++ Defining Classes - Lab/01. Bank Account/BankAccount.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class BankAccount
+{
+
+    public int Id { get; set; }
+    public decimal Balance { get; set; }
+
+}
+
