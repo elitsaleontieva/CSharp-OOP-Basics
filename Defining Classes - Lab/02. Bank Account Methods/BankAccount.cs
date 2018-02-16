bc07b52a6a2d5072430a924d98e17d740752aedd
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-16 18:48:54 +0200

    Add files via upload

---
+++ Defining Classes - Lab/02. Bank Account Methods/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class BankAccount
+{
+
+    public int Id { get; set; }
+    public decimal Balance { get; set; }
+
+//    •	Deposit(decimal amount): void
+//•	Withdraw(decimal amount): void
+
+    public void Deposit(decimal amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        this.Balance -= amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Account {this.Id}, balance {this.Balance}";
+    }
+}
+
