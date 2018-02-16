8f8ffac8955e0a5a22b85aa1d578921d32aa4147
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-16 22:49:52 +0200

    Add files via upload

---
+++ Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Family
+{
+    public List<Person> people { get; set; }
+
+    public Family()
+    {
+        this.people = new List<Person>();
+    }
+
+    public void AddMember(Person member)
+    {
+        this.people.Add(member);
+    }
+
+    public Person GetOldestMember()
+    {
+        return this.people
+            .OrderByDescending(x => x.Age)
+            .FirstOrDefault();
+    } 
+}
+
---
+++ Defining Classes - Exercise/03. Oldest Family Member/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person()
+    {
+
+    }
+
+    
+
+
+}
+
