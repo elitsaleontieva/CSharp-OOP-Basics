bedc201ff662629cb9093ba29680c58e6ecd0531
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-16 22:07:32 +0200

    Create 01. Define a Class Person.cs

---
+++ Defining Classes - Exercise/01. Define a Class Person.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+
+    public Person()
+    {
+
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+}
