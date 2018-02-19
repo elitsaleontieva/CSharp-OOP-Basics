74d6ea4bac25b65918fdd5ab8c3030041265e5af
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-19 22:27:34 +0200

    Update Program.cs

--- Defining Classes - Exercise/07. Speed Racing/Program.cs
+++ Defining Classes - Exercise/07. Speed Racing/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -8,8 +8,8 @@ public class Program
     {
         var n = int.Parse(Console.ReadLine());
         var list = new List<Car>();
-
         var distanceTraveled = 0;
+        
         for (int i = 0; i < n; i++)
         {
 
@@ -21,35 +21,34 @@ public class Program
             var fuelConsumptionFor1km = double.Parse(parts[2]);
 
             Car car = new Car();
+            
             car.Model = model;
-            car.FuelAmount = fuelAmount; //23
-            car.FuelConsumptionFor1Km = fuelConsumptionFor1km; //0.3
-            car.DistanceTraveled = distanceTraveled; //0
-                                                     //AudiA4 23 0.3
+            car.FuelAmount = fuelAmount; 
+            car.FuelConsumptionFor1Km = fuelConsumptionFor1km;
+            car.DistanceTraveled = distanceTraveled; 
+                                                     
             list.Add(car);
         }
-            var command = Console.ReadLine();
+        
+        var command = Console.ReadLine();
 
         while (command != "End")
-            {
+        {
             foreach (var car in list)
             {
-                
+
                 var chars = " ".ToArray();
                 var parts2 = command.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+                
                 var carModel = parts2[1];
                 var amountOfKm = double.Parse(parts2[2]);
-
                 var totalConsumption = car.FuelConsumptionFor1Km * amountOfKm;
+                
                 if (carModel == car.Model)
                 {
-
-
                     if (totalConsumption > car.FuelAmount)
                     {
                         Console.WriteLine("Insufficient fuel for the drive");
-                
-
                     }
                     else if (totalConsumption == car.FuelAmount)
                     {
@@ -59,25 +58,20 @@ public class Program
                     else
                     {
                         car.FuelAmount -= totalConsumption;
-
                         car.DistanceTraveled -= amountOfKm;
-                        
+
                     }
                 }
-               
-
             }
-                command = Console.ReadLine();
-            }
+            
+            command = Console.ReadLine();
+        }
 
         foreach (var car in list)
         {
-           
             Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {Math.Abs(car.DistanceTraveled)}");
         }
 
 
     }
-    }
-
-
+}
