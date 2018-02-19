41c83a1ce5f502bff0ba40749fa5f92ee546a4bb
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-19 22:24:53 +0200

    Create Car.cs

---
+++ Defining Classes - Exercise/07. Speed Racing/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Car
+{
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelConsumptionFor1Km { get; set; }
+    public double DistanceTraveled { get; set; }
+}
+
