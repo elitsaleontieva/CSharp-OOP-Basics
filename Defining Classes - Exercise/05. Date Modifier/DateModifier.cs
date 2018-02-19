ea4dd0bb84b9019d770b3fc55dcf878779319ce2
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-19 18:50:46 +0200

    Add files via upload

---
+++ Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class DateModifier
+{
+    public string FirstDate { get; set; }
+    public string SecondDate { get; set; }
+
+    public DateModifier()
+    {
+
+    }
+
+    public string DifferenceBetweenDates(string date1, string date2)
+    {
+        var parseDate1 = DateTime.Parse(date1);
+        var parseDate2 = DateTime.Parse(date2);
+
+        return $"{(parseDate1 - parseDate2).TotalDays}";
+    }
+
+}
---
+++ Defining Classes - Exercise/05. Date Modifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var date1 = Console.ReadLine();
+        var date2 = Console.ReadLine();
+
+        DateModifier dateModifier = new DateModifier();
+        dateModifier.FirstDate = date1;
+        dateModifier.SecondDate = date2;
+
+        Console.WriteLine($"{Math.Abs(int.Parse(dateModifier.DifferenceBetweenDates(date1, date2)))}");
+
+    }
+}
