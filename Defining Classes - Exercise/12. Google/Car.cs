394725d52b46590ce363a431bc818975592580ac
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-21 19:14:50 +0200

    Create Car.cs

---
+++ Defining Classes - Exercise/12. Google/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Car
+{
+    public string Name { get; set; }
+    public string CarModel { get; set; }
+    public int CarSpeed { get; set; }
+
+}
+
