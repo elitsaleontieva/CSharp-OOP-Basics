969f26447df9e66dd030103ce2fef550a47da236
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-21 18:05:42 +0200

    Create Car.cs

---
+++ Defining Classes - Exercise/10. Car Salesman/Car.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+    public class Car
+    {
+    public string Model { get; set; }
+    public string Engine { get; set; }
+    public double Weight { get; set; }
+    public string Color { get; set; }
+}
+
