f79ff1cd669b5df9d3acd2dbd71ac846aa1aae4c
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-28 00:44:14 +0200

    Add files via upload

---
+++ 04. Team/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var team = new Team("Name");
+
+            for (int i = 0; i < n; i++)
+            {
+                var input= Console.ReadLine();
+                var parts = input.Split(' ');
+                var firstName = parts[0];
+                var secondName = parts[1];
+                var age = int.Parse(parts[2]);
+                var salary = decimal.Parse(parts[3]);
+
+                Person person = new Person(firstName,secondName,age,salary);
+
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players");
+
+        }
+    }
+}
---
+++ 04. Team/Team.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(string name)
+    {
+        this.name = name;
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+    }
+
+    public List<Person> FirstTeam{ get { return this.firstTeam; } }
+    public List<Person> ReserveTeam { get { return this.reserveTeam; } }
+
+
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age<40)
+        {
+            firstTeam.Add(person);
+        }
+        else
+        {
+            reserveTeam.Add(person);
+        }
+    }
+
+
+
+}
+
