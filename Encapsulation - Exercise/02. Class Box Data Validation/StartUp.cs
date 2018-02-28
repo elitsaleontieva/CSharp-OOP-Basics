2e12a073b91aebf02b9b6e2c3a4072e0eedf8735
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-28 17:47:58 +0200

    Add files via upload

---
+++ Encapsulation - Exercise/02. Class Box Data Validation/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+        try
+        {
+            Box box = new Box(length, width, height);
+
+            Console.WriteLine(box.ToString());
+
+        }
+        catch (ArgumentException exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+
+    }
+}
