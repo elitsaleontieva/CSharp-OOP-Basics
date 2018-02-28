96da99e4482302439fdf7ae3a905170ee7c9d31c
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-02-28 16:30:50 +0200

    Add files via upload

---
+++ Encapsulation - Exercise/01. Class Box/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length,width,height);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
