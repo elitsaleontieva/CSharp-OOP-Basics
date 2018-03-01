f064d103d74761d1f48397246fce1e95c9d242ae
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-02 00:49:44 +0200

    Create Animal.cs

---
+++ Inheritance - Lab/03. Hierarchical Inheritance/Animal.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+
+    }
