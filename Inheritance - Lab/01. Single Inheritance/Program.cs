7b3652d2633c8f31146da1093d22cef10eeb460f
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-01 17:29:45 +0200

    Create Program.cs

---
+++ Inheritance - Lab/01. Single Inheritance/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Animal animal = new Animal();
+
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+
