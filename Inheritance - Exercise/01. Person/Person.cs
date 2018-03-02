6d320541c023760d699687f0a707b32345099589
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-02 18:39:24 +0200

    Create Person.cs

---
+++ Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private int age;
+    private string name;
+
+
+    public virtual int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            if (value<0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+            else
+            {
+                this.age = value;
+            }
+        }
+    }
+
+    public virtual string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+            this.name = value;
+        }
+    }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.Name}, Age: {this.Age}";
+    }
+
+
+}
+
