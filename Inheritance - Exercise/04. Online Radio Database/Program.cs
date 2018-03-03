15dc99cef73c204458504a8ab1fb0be81a858b5d
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-03 21:35:59 +0200

    Add files via upload

---
+++ Inheritance - Exercise/04. Online Radio Database/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+            var n = int.Parse(Console.ReadLine());
+
+            var listOfSongs = new List<Song>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var chars = ";".ToCharArray();
+            
+                var parts = input.Split(chars, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            if (parts.Length == 3)
+            {
+                try { 
+                var artistName = parts[0];
+                        var songName = parts[1];
+                        var length = parts[2];
+                        var lengthParts = parts[2].Split(':').ToArray();
+                        var minutes = int.Parse(lengthParts[0]);
+                        var seconds = int.Parse(lengthParts[1]);
+
+                        Song song = new Song(artistName, songName, minutes, seconds, length);
+
+                    Console.WriteLine("Song added.");
+                    listOfSongs.Add(song);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+            }
+
+        var totalSeconds = listOfSongs.Select(x => x.Seconds).Sum() + listOfSongs.Select(x => x.Minutes).Sum() * 60;
+        TimeSpan t = TimeSpan.FromSeconds(totalSeconds);
+        
+        
+
+        Console.WriteLine($"Songs added: {listOfSongs.Count}");
+
+
+        Console.WriteLine($"Playlist length: {t.Hours}h {t.Minutes}m {t.Seconds}s");
+        
+
+        
+
+
+    }
+}
+
---
+++ Inheritance - Exercise/04. Online Radio Database/Song.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Song
+{
+    private string artistName;
+    private string songName;
+    private int minutes;
+    private int seconds;
+    private string length;        
+    
+    public string ArtistName
+    {
+        get
+        {
+            return this.artistName;
+        }
+        set
+        {
+            if (value.Length<3 || value.Length>20)
+            {
+                throw new InvalidArtistNameException();
+            }
+            else if (value == null)
+            {
+                throw new InvalidSongException();
+            }
+            else
+            {
+                this.artistName = value;
+            }
+        }
+    }
+    public string SongName
+    {
+        get
+        {
+            return this.songName;
+        }
+        set
+        {
+            if (value.Length<3 || value.Length>30)
+            {
+                throw new InvalidSongNameException();
+            }
+            else if (value == null)
+            {
+                throw new InvalidSongException();
+            }
+            else
+            {
+                this.songName = value;
+            }
+        }
+    }
+    public int Minutes
+    {
+        get
+        {
+            return this.minutes;
+        }
+        set
+        {
+            if (value<0 || value>14)
+            {
+                throw new InvalidSongMinutesException();
+            }
+            else
+            {
+                this.minutes = value;
+            }
+        }
+    }
+    public int Seconds
+    {
+        get
+        {
+            return this.seconds;
+        }
+        set
+        {
+            if (value<0 || value>59)
+            {
+                throw new InvalidSongSecondsException();
+            }
+            else
+            {
+                this.seconds = value;
+            }
+        }
+    }
+
+    public string Length
+    {
+
+        get
+        {
+            return this.length;
+        }
+        set
+        {
+            var minutesAndSeconds = value.Split(':').ToArray();
+            if (int.Parse(minutesAndSeconds[0])<0 || int.Parse(minutesAndSeconds[0])>14  
+                || int.Parse(minutesAndSeconds[1])>59 || int.Parse(minutesAndSeconds[1])<0)
+            {
+                throw new InvalidSongLengthException();
+            }
+            else if (value == null)
+            {
+                throw new InvalidSongException();
+            }
+            else
+            {
+                this.length = value;
+            }
+        }
+    }
+
+
+
+    public Song(string artistName, string songName, int minutes, int seconds, string length)
+    {
+        this.ArtistName = artistName;
+        this.SongName = songName;
+        this.Minutes = minutes;
+        this.Seconds = seconds;
+        this.Length = length;
+    }
+
+}
+
