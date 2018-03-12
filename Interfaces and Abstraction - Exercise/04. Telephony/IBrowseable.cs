df87a9d87c8ce44e8f603eba704a34c8245a7134
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-12 23:12:42 +0200

    Add files via upload

---
+++ Interfaces and Abstraction - Exercise/04. Telephony/IBrowseable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    public interface IBrowseable
+    {
+        string BrowseWeb(string site);
+    }
+
---
+++ Interfaces and Abstraction - Exercise/04. Telephony/Smartphone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Smartphone : ICallable, IBrowseable
+{
+
+    public string Site { get; set; }
+    public string Number { get; set; }
+
+
+    public string BrowseWeb(string site)
+    {
+        if (!site.ToCharArray().Any(x => Char.IsDigit(x)))
+        {
+            return $"Browsing: {site}!";
+        }
+        else
+        {
+            throw new ArgumentException("Invalid URL!");
+        }
+    }
+
+    public string CallPhone(string phone)
+    {
+        if (phone.ToCharArray().All(x => Char.IsDigit(x)))
+        {
+            return $"Calling... {phone}";
+        }
+        else
+        {
+            throw new ArgumentException("Invalid number!");
+
+        }
+    }
+
+
+}
