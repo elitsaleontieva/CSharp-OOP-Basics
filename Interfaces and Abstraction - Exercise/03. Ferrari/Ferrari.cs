a57e043b40f11a4baa09b6f3a372cd38d1946800
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-12 21:32:47 +0200

    Add files via upload

---
+++ Interfaces and Abstraction - Exercise/03. Ferrari/Ferrari.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Ferrari : ICar
+{
+    public string Model = "488-Spider";
+    public string DriversName { get; set; }
+
+    public Ferrari(string driversName)
+    {
+        this.DriversName = driversName;
+    }
+
+    public string GasPedalPushed()
+    {
+        return "GasPedalPushed";
+    }
+
+    public string PushBreaks()
+    {
+        return "Brakes!";
+    }
+
+
+}
+
